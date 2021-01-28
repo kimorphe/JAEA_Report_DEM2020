e8d483d3ac7c493055c2057004132763e4457407
kazushi <kimoto@okayama-u.ac.jp>
2021-01-28 22:55:11 +0900

    sumitted version

--- cmp_pdf.sh
+++ cmp_pdf.sh
@@ -1,2 +1,2 @@
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=repot2019DEM.pdf report.pdf
+gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -dNOPAUSE -dQUIET -dBATCH -sOutputFile=repot2020DEM.pdf report.pdf
 # PDFSETTINGS options: /default, /screen, /ebook, /printer, /prepress
